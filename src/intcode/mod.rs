a1f7df769cc00de16996ec859e77f7c84032614b
Kevin Chen <kvchen@fb.com>
2019-12-19 01:58:36 -0800

    Remove debug code from intcode

--- src/intcode/mod.rs
+++ src/intcode/mod.rs
@@ -259,22 +259,3 @@ impl Computer {
         self.memory[idx] = value;
     }
 }
-
-// impl Iterator for Computer {
-//     type Item = i64;
-
-//     // Here, we define the sequence using `.curr` and `.next`.
-//     // The return type is `Option<T>`:
-//     //     * When the `Iterator` is finished, `None` is returned.
-//     //     * Otherwise, the next value is wrapped in `Some` and returned.
-//     fn next(&mut self) -> Option<u32> {
-//         let new_next = self.curr + self.next;
-
-//         self.curr = self.next;
-//         self.next = new_next;
-
-//         // Since there's no endpoint to a Fibonacci sequence, the `Iterator`
-//         // will never return `None`, and `Some` is always returned.
-//         Some(self.curr)
-//     }
-// }
